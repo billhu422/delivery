dfef81f4272fdde3cf4077f12d6b7f7831cff34a
xiaowei hu <billhu422@126.com>
2017-07-21 04:40:03 +0000

    add resp.status(xxx)

--- qcloud.js
+++ qcloud.js
@@ -46,13 +46,13 @@ router.post('/cvm',function(req, res) {
 
 router.post('/bgpip/checkCreate',function(req,resp){
     console.log(req.body);
-    if(req.get('Authorization') == undefined) {resp.send('{"code" : -8,"description" : "Need Authorization"}');return;}
+    if(req.get('Authorization') == undefined) {resp.status(400);resp.send('{"code" : -8,"description" : "Need Authorization"}');return;}
     var access_token = req.get('Authorization').split(" ")[1];
     var url = config.oauth.account_server + '/user';
     console.log("url : " + url);
     oauth_client.get(url, access_token,function(e,response){ 
-    	if(e) {resp.send(e);return;}
-    	if(JSON.parse(response).id == undefined ) {resp.send('{"code" : -1,"description" : "not found user id from idm with access_token"}');return;}
+    	if(e) {resp.status(401);resp.send('{"code" : -9,"description" : "invalid auth-token"}');return;}
+    	if(JSON.parse(response).id == undefined ) {resp.status(400);resp.send('{"code" : -1,"description" : "not found user id from idm with access_token"}');return;}
 	
 	var capi = new Capi({
     		SecretId: config.qcloud.SecretId,
@@ -81,15 +81,15 @@ router.post('/bgpip/checkCreate',function(req,resp){
 router.post('/bgpip/create',function(req,resp){
     console.log(req.body);
     console.log(req.get('Authorization'));
-    if(req.get('Authorization') == undefined) {resp.send('{"code" : -8,"description" : "Need Authorization"}');console.log('{"code" : -8,"description" : "Need Authorization"}');return;}
+    if(req.get('Authorization') == undefined) {resp.status(400);resp.send('{"code" : -8,"description" : "Need Authorization"}');console.log('{"code" : -8,"description" : "Need Authorization"}');return;}
     var access_token = req.get('Authorization').split(" ")[1];
     var url = config.oauth.account_server + '/user';
     //1. validte customer access token 
     oauth_client.get(url, access_token,function(e,response){
     console.log(JSON.stringify(response,4,4));
-        if(e) {resp.send(e);return;}
+        if(e) {resp.status(401);resp.send('{"code" : -9,"description" : "invalid auth-token"}');return;}
         console.log('Retrieving userid...');
-        if(JSON.parse(response).id == undefined ) {resp.send('{"code" : -1,"description" : "not found user id from idm with access_token"}');console.log('{"code" : -1,"description" : "not found user id from idm with access_token"}');return;}
+        if(JSON.parse(response).id == undefined ) {resp.status(400);resp.send('{"code" : -1,"description" : "not found user id from idm with access_token"}');console.log('{"code" : -1,"description" : "not found user id from idm with access_token"}');return;}
         //1.0 retrieve userid with access_token
         var userId = JSON.parse(response).id;
 
@@ -114,11 +114,11 @@ router.post('/bgpip/create',function(req,resp){
                 var orders = JSON.parse(body);
                 var order = orders[0];
 	        console.log(order);	
-                if( order == undefined) {resp.send('{"code" : -2,"description" : "Not found order."}');return;}
+                if( order == undefined) {resp.status(400);resp.send('{"code" : -2,"description" : "Not found order."}');return;}
  	        
                 //validate pay status
                 orderStatus = order.note.filter(function(x){return x.text=="Paid"})[0].text;
-                if(orderStatus != "Paid") {resp.send('{"code":-3,"description":"The order is not paied, cannot  delivery an instance by the order"}');console.log('{"code":-3,"description":"The order is not paied, cannot  delivery an instance by the order"}');return;}
+                if(orderStatus != "Paid") {resp.status(400);resp.send('{"code":-3,"description":"The order is not paied, cannot  delivery an instance by the order"}');console.log('{"code":-3,"description":"The order is not paied, cannot  delivery an instance by the order"}');return;}
 	
 		//delivery instances
                 order.orderItem.forEach(function(item){
@@ -126,8 +126,8 @@ router.post('/bgpip/create',function(req,resp){
                 //failed ->hold
 			//retrieve itemUserId
 			var itemPartyId = item.product.relatedParty.filter(function(x){return x.role=="Customer"})[0].id;	
-			if(itemPartyId == undefined) {resp.send('{"code":-4,"description":"Cannot found itemPartyId"}');console.log('{"code":-4,"description":"Cannot found itemPartyId"}');return;}
-			if(item.state != 'Acknowledged' || item.state != "InProgess")  {resp.send('{"code":-7,"description":"Only Acknowledged or InProgess can be manually modified"}');console.log('{"code":-7,"description":"Only Acknowledged or InProgess can be manually modified"}');return;}
+			if(itemPartyId == undefined) {resp.status(400);resp.send('{"code":-4,"description":"Cannot found itemPartyId"}');console.log('{"code":-4,"description":"Cannot found itemPartyId"}');return;}
+			if(item.state != 'Acknowledged' || item.state != "InProgess")  {resp.status(400);resp.send('{"code":-7,"description":"Only Acknowledged or InProgess can be manually modified"}');console.log('{"code":-7,"description":"Only Acknowledged or InProgess can be manually modified"}');return;}
                          
                         var provider = item.product.productCharacteristic.filter(function(x){return x.name=="provider"})[0].value;		        
                         var productName = item.product.productCharacteristic.filter(function(x){return x.name=="productname"})[0].value;
@@ -214,6 +214,8 @@ router.post('/bgpip/create',function(req,resp){
                                                                                 console.log("xxx");
                                                                                 console.log(body);
                                                                                 if(patchordererr){ resp.send(patchordererr);return; }
+										resp.status(500);
+                                                                                console.log('{"code":45,description:"Retreive adminAccessToken Error"');
                                                                                 resp.send('{"code":-5,description:"Retreive adminAccessToken Error"');
                                                                         });
 					}
@@ -237,6 +239,8 @@ router.post('/bgpip/create',function(req,resp){
                                                                 request.patch(options, function(patchordererr, response, body){
                                                                                 console.log(body);
                                                                                 if(patchordererr){ resp.send(patchordererr);return; }
+										resp.status(500);
+                                                                                console.log('{"code":-6,"description":"Write instance to inventory database failed"');
                                                                                 resp.send('{"code":-6,"description":"Write instance to inventory database failed"');
                                                                         });								
 							}
@@ -268,115 +272,4 @@ router.post('/bgpip/create',function(req,resp){
 	   });
     });
 });
-
-router.post('/bgpip/create1',function(req,resp){
-    console.log(req.body);
-    var access_token = req.get('Authorization').split(" ")[1];
-    var url = config.oauth.account_server + '/user';
-    //1. validte customer access token 
-    oauth_client.get(url, access_token,function(e,response){ 
-	console.log(JSON.stringify(response,4,4));
-    	if(e) {resp.send(e);return;}
-	console.log('Retrieving userid...');
-    	if(JSON.parse(response).id == undefined ) {resp.send('{"code" : -1,"description" : "not found user id from idm with access_token"}');return;}
-        //1.0 retrieve userid with access_token
-        var userId = response.id; 
-	//1.1 retrieve order info(orderItemId,order related partyid, order status)
-        console.log('Retrieve order info...');
-        var orderId = req.body.orderId;
-        var orderItemId = req.body.orderItemId;
-        var partyId;
-        var provider;
-        var productName;
-        var orderStatus;
-        var options = {
-                url: config.eco.baseUrl + config.eco.orderPath + "/" + orderId + "?" + "orderItemId=" + orderItemId,
-                headers: {
-                        'Authorization': req.get('Authorization')
-                }
-        };
-        request(options,function(error,response,body){
-		console.log(JSON.stringify(body,4,4));
-                if(error) {resp.send(error);return;}
-	        	
-		//retrieve order property
-                //a.partyId
-                var order = JSON.parse(body);
-                orderId = order.id;
-                for(party in order.relatedParty) {
-                	if(party.role == "Customer") {paryId = party.id; break;}
-                }
-                //b.provider
-                provider = "qcloud";
-                //c.productName
-                productName = "gaofangip";
-                //d.orderStatus;
-                orderStatus = "paied";
-               	
-                //2. validate userid with order's related partyid
-                console.log("Validating userid with order's related partyid..."); 
-        	if( userId != partyId) {resp.send('{"code":-2,"description":"The User is not the customer in order, so cannot  delivery   instance by the order"}');return;}
-                console.log("Validate userid successfully!");
-        	//3. check order status whether or not paid
-                console.log("Checking order status...");
-        	if( orderStatus != "paied") {resp.send('{"code":-3,"description":"The order  is not paied, so cannot  delivery   instance by the order"}');return;}
-                console.log("Check order status successfully!");
-        	console.log("Delivery now!");
-        	//5. if delievery successfully, then write instance info into inventory database, or not return error.
-                request.post({
-  			headers: {'content-type' : 'application/x-www-form-urlencoded'},
-  			url:     config.dbRest.baseUrl + '/inventory/instance',
-  			form:    {'orderId':orderId,'orderItemId':orderItemId,'userId':'huxiaowei','provider':'cvm','productName':'cvm','instanceId':'005'}
-		}, function(error, response, body){
-			if(error) {resp.send(error);return;}
-                	
-			//6. update order status to completed.
-                        //6.1 retrieve seller access_token
-                        console.log("write database successfully!");
-			//request.patch();
-			resp.send("write database successfully!");
-                          
-		});
-        });
-
-
-        //4. delivery product
-/*	var capi = new Capi({
-    		SecretId: config.qcloud.SecretId,
-    		SecretKey: config.qcloud.SecretKey,
-    		serviceType: 'account'
-    	});
-     	
-	var params = assign({    
-     	Region: req.query.region,
-        Action: 'BgpipCheckCreate'},req.body);
-
-     	capi.request(params, {
-    				serviceType: 'bgpip'
-				}, function(error, data) {
-    					console.log(JSON.stringify(data,4,4));
-                			if(error) resp.send(error);
-                			else {
-                                            //5. if delievery successfully, then write instance info into inventory database, or not return error.
-        				    //6. update order status to completed.
-                     			    }
-				});
-*/
-     });
-});
-
-router.get('/order',function(req,resp){
-        var options = {
-  		url: config.eco.baseUrl + config.eco.orderPath,
-  		headers: {
-    			'Authorization': req.get('Authorization')
-  		}
-	};
-	request(options,function(error,response,body){
-                if(error) {resp.send(error);return;}
-                console.log(body);
-        });
-});
-
-
 module.exports = router;
